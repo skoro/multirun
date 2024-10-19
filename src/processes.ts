59a6f993df0ff60037cf0fbcc1226838ba20ec15
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-10-19 18:59:38 +0300

    ref + restart process

--- src/processes.ts
+++ src/processes.ts
@@ -18,32 +18,41 @@ type ProcessEntry = {
 
 let processTable: ProcessTable = {}
 
-function spawnProcesses(processesConfig: ProcessEntriesConfig, logger: Logger) {
+function spawnProcesses(processesConfig: ProcessEntriesConfig, logger: Logger): void {
   logger.debug('Spawn procecesses');
 
   processTable = {};
 
-  for (const name of Object.keys(processesConfig)) {
-    startProcess(name, processesConfig[name]);
-  }
+  for (const procName of Object.keys(processesConfig)) {
+    const procConfig = processesConfig[procName];
 
-  if (! hasMoreProcesses()) {
-    throw new Error('');
+    startProcess(procName, procConfig, logger);
   }
 }
 
-function startProcess(name: string, config: ProcessConfig): ChildProcess {
-  if (!isValidCommand(config?.command)) {
+function startProcess(procName: string, procConfig: ProcessConfig, logger: Logger): ChildProcess {
+  logger.debug(procConfig);
+
+  if (!isValidCommand(procConfig?.command)) {
     throw new Error('Process command must be a non empty string.');
   }
 
-  const command = config.command!;
-  const args = config.args ?? [];
-  const options = getSpawnOptions(config);
+  const command = procConfig.command!;
+  const args = procConfig.args ?? [];
+  const options = getSpawnOptions(procConfig);
 
   const proc = spawn(command, args, options);
 
-  addProcessEntry(name, config, proc);
+  proc.on('spawn', () => {
+    addProcessEntry(procName, procConfig, proc);
+    logger.info(`"${procName}" started, pid: ${proc.pid}`);
+  });
+
+  proc.on('error', (err) => {
+    logger.error(`"${procName}": ${err}`);
+  });
+
+  proc.on('close', (code: number) => closeOrRestartProcess(procName, code, proc.pid!, logger));
 
   return proc;
 }
@@ -62,16 +71,58 @@ function getSpawnOptions(config: ProcessConfig): SpawnOptions {
   return options;
 }
 
-function addProcessEntry(name: string, config: ProcessConfig, proc: ChildProcess): void {
-  if (!processTable[name]) {
-    processTable[name] = {}
+function addProcessEntry(procName: string, procConfig: ProcessConfig, proc: ChildProcess): void {
+  if (!processTable[procName]) {
+    processTable[procName] = {}
   }
 
-  processTable[name][proc.pid!] = { config, proc }
+  if (proc.pid) {
+    processTable[procName][proc.pid] = {
+      config: procConfig,
+      proc,
+    }
+  }
+}
+
+function deleteProcessEntryByPid(procName: string, pid: number): boolean {
+  if (hasProcessEntry(procName, pid)) {
+    delete processTable[procName][pid];
+    return true;
+  }
+
+  return false;
+}
+
+function getProcessEntryByPid(procName: string, pid: number): ProcessEntry | null {
+  if (hasProcessEntry(procName, pid)) {
+    return processTable[procName][pid];
+  }
+
+  return null;
+}
+
+function hasProcessEntry(procName: string, pid: number): boolean {
+  return processTable[procName]
+    && (pid.toString() in processTable[procName]);
 }
 
 function hasMoreProcesses(): boolean {
   return Object.keys(processTable).length > 0;
+}
+
+function closeOrRestartProcess(procName: string, code: number, pid: number, logger: Logger): void {
+  logger.warn(`"${procName}" exit, code: ${code}`);
+
+  const procEntry = getProcessEntryByPid(procName, pid);
+
+  if (!deleteProcessEntryByPid(procName, pid)) {
+    logger.debug(`deleteProcessEntry("${procName}", ${pid}) failed`);
+  }
+
+  if (procEntry?.config.restart) {
+    logger.info(`"${procName}" restart`);
+    startProcess(procName, procEntry.config, logger);
+  }
 }
 
 export { spawnProcesses, startProcess }
