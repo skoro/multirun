369aff91891a350f89f85616a30563e4efc83511
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2024-11-09 15:21:50 +0200

    #11 helper for mocking ChildProcess

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,5 +1,17 @@
 import path from 'node:path';
+import child_process from 'node:child_process';
+import EventEmitter from 'node:events';
+import { Readable } from 'node:stream';
 
 export function getAssetPath(assetName: string): string {
   return path.join(__dirname, '..', '__tests__', 'assets', assetName);
+}
+
+export function createChildProcessMock(): child_process.ChildProcess {
+  const proc = new EventEmitter() as child_process.ChildProcess;
+
+  proc.stdout = new EventEmitter() as Readable;
+  proc.stderr = new EventEmitter() as Readable;
+
+  return proc;
 }
